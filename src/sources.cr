8a0966063eaf729d1da17faf31d640fffc65e2d6
Alastair Codd <alastaircodd@hotmail.co.uk>
2022-01-26 01:20:13 +0000

    Hopefully improve data fetching

--- src/sources.cr
+++ src/sources.cr
@@ -23,6 +23,12 @@ class SourceData
   end
 end
 
+def get_top_of_next_hour(time : Time) : Time
+  result = Time.utc(time.year, time.month, time.day, time.hour, 0, 0)
+  result += Time::Span.new(hours: 1)
+  return result
+end
+
 abstract class Source
   abstract def add_client
   abstract def rm_client
@@ -35,26 +41,18 @@ class CompositeLiveSource < Source
   property ident : String
   property clients : Int32 = 0
   property current_data : SourceData
-  property running : Bool = true
+  property running : Bool = false
+
+  property last_data_fetch_time = Time.utc
+
+  getter loop_frequency_seconds : UInt8 = 2
+  getter sim_fetch_frequency_seconds : UInt32 = 3600
+  getter team_fetch_frequency_seconds : UInt32 = 60
 
   def initialize(@ident : String, @tx : Channel({String, SourceData}))
     @current_data = SourceData.new
 
-    spawn do
-      while @running
-        @current_data.sim = get_sim
-        break if @current_data.sim.nil?
-        current_sim = @current_data.sim.not_nil!
-        break if Time::Format::ISO_8601_DATE_TIME.parse(current_sim["simEnd"].to_s) < Time.utc
-
-        @current_data.teams = get_teams || @current_data.teams
-        @current_data.games = get_games current_sim["day"].as_i, current_sim["season"].as_i, current_sim["id"].as_s
-        @tx.send({@ident, @current_data})
-        sleep 2.seconds
-      end
-
-      pp "loop ended at time #{Time.utc}"
-    end
+    start
   end
 
   def add_client
@@ -67,6 +65,86 @@ class CompositeLiveSource < Source
 
   def n_clients
     @clients
+  end
+
+  def start
+    if @running
+      return
+    end
+
+    @running = true
+
+    spawn do
+      begin
+        counter = 0
+
+        while @running
+          any_updates = false
+          if counter % @sim_fetch_frequency_seconds == 0
+            new_sim = get_sim
+            if !new_sim.nil?
+              Log.trace { "live sim data fetched" }
+              is_sim_data_different = new_sim != @current_data.sim
+              any_updates |= is_sim_data_different
+              Log.trace { "  is_sim_data_different=#{is_sim_data_different}" }
+              @current_data.sim = new_sim
+            end
+          end
+
+          if @current_data.sim.nil?
+            Log.error { "could not get data for sim" }
+            break
+          end
+          current_sim = @current_data.sim.not_nil!
+          @last_data_fetch_time = Time.utc
+          if Time::Format::ISO_8601_DATE_TIME.parse(current_sim["simEnd"].to_s) < @last_data_fetch_time
+            Log.info { "sim has ended" }
+            break
+          end
+
+          if counter % @team_fetch_frequency_seconds == 0
+            new_teams = get_teams
+            if !new_teams.nil?
+              Log.trace { "live team data fetched" }
+              is_team_data_different = new_teams != @current_data.teams
+              any_updates |= is_team_data_different
+              Log.trace { "  is_team_data_different=#{is_team_data_different}" }
+              @current_data.teams = new_teams
+            end
+          end
+
+          new_games = get_games current_sim["day"].as_i, current_sim["season"].as_i, current_sim["id"].as_s
+          if !new_games.nil?
+            Log.trace { "live game data fetched" }
+            is_game_data_different = new_games != @current_data.games
+            any_updates |= is_game_data_different
+            Log.trace { "  is_game_data_different=#{is_game_data_different}" }
+            @current_data.games = new_games
+          end
+
+          if any_updates
+            Log.trace { "drawing to client" }
+            @tx.send({@ident, @current_data})
+          end
+
+          if !@current_data.games.nil? && @current_data.games.not_nil!.all? { |g| g["gameComplete"].as_bool }
+            @last_data_fetch_time = Time.utc
+
+            Log.debug { "sleeping until top of next hour" }
+            counter = 0
+            sleep get_top_of_next_hour(@last_data_fetch_time) - @last_data_fetch_time
+          else
+            counter += @loop_frequency_seconds
+            sleep @loop_frequency_seconds.seconds
+          end
+        end
+      rescue ex
+        Log.error(exception: ex) {}
+      ensure
+        Log.info { "loop ended at time #{Time.utc}" }
+        @running = false
+      end
+    end
   end
 
   def close
@@ -100,14 +178,14 @@ class CompositeLiveSource < Source
       messages = JSON.parse response.body
       return messages["items"]
     else
-      puts "http request failed"
-      pp url
-      pp response.status_code
+      Log.error { "http request failed" }
+      Log.error { url }
+      Log.error { response.status_code }
       return
     end
   end
 
-  def get_games(day : Int32, season : Int32, sim : String) : Array(JSON::Any)
+  def get_games(day : Int32, season : Int32, sim : String) : Array(JSON::Any)?
     url = URI.parse(ENV["CHRONICLER_URL"] ||= "https://api.sibr.dev/chronicler/")
     url.query = URI::Params.encode({"day" => day.to_s, "season" => season.to_s, "sim" => sim})
     url.path = (Path.new(url.path) / "v1" / "games").to_s
@@ -121,7 +199,7 @@ class CompositeLiveSource < Source
       puts "http request failed"
       pp url
       pp response.status_code
-      return Array(JSON::Any).new
+      return
     end
   end
 
