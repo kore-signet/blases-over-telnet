5317787ca83cceafa37c6782282f49b853bd5468
Alastair Codd <alastaircodd@hotmail.co.uk>
2022-09-04 13:30:36 +0100

    Add spec for historic playback to verify game advancement assumptions

--- spec/historic_playback_spec.cr
+++ spec/historic_playback_spec.cr
@@ -1,3 +1,4 @@
+require "spec"
 require "../src/sources/historic_source.cr"
 
 def get_historic_source : ChroniclerSource
@@ -13,7 +14,9 @@ def get_historic_source : ChroniclerSource
     "timestamp" => JSON.parse(%("2021-06-27T07:00:00Z")),
     "data"      => JSON.parse(%({
         "day": 12,
-        "gameComplete": false
+        "gameStart": true,
+        "gameComplete": false,
+        "lastUpdate": "Play Ball!"
       })),
   }
 
@@ -22,14 +25,18 @@ def get_historic_source : ChroniclerSource
       "timestamp" => JSON.parse(%("2021-06-27T07:01:00Z")),
       "data"      => JSON.parse(%({
         "day": 12,
-        "gameComplete": false
+        "gameStart": true,
+        "gameComplete": false,
+        "lastUpdate": "Game canceled. Neither team non-lost."
       })),
     },
     {
       "timestamp" => JSON.parse(%("2021-06-27T07:02:00Z")),
       "data"      => JSON.parse(%({
         "day": 12,
-        "gameComplete": false
+        "gameStart": true,
+        "gameComplete": false,
+        "lastUpdate": "Game over."
       })),
     },
   ]
@@ -38,6 +45,10 @@ def get_historic_source : ChroniclerSource
 end
 
 describe "get_time_next_data_expires" do
+  Spec.before_suite do
+    Log.setup(:trace)
+  end
+
   it "current time" do
     get_historic_source.current_time.should eq Time.utc(2021, 6, 27, 7, 0, 0)
   end
@@ -50,7 +61,14 @@ describe "get_time_next_data_expires" do
 
   it "update current game event" do
     historic_source = get_historic_source
+    historic_source.current_time = historic_source.get_time_that_next_data_expires
     historic_source.update_current_game_event_for_all_ongoing_games
+    historic_source.update_current_data
+    Log.info { "updated current game event" }
+
+    current_game : GameData = historic_source.current_data.games.not_nil![0]
+    current_game["timestamp"].as_s.should eq "2021-06-27T07:01:00Z"
+    current_game["data"]["lastUpdate"].as_s.should eq "Game canceled. Neither team non-lost."
     historic_source.current_time.should eq Time.utc(2021, 6, 27, 7, 1, 0)
     historic_source.get_time_that_next_data_expires.should eq Time.utc(2021, 6, 27, 7, 2, 0)
   end
